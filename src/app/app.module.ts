07fa2c1691bc02312a9c5657c262f697803d023a
Daniel Brown <djbrown@users.noreply.github.com>
2020-12-15 13:45:59 +0100

    display playlists in material expansion panels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -30,6 +31,7 @@ import { httpInterceptorProviders } from './http-interceptors/index';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatExpansionModule,
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
